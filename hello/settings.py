bb011c3b9e49694221802e9e5ecaa977433f0c9b
anirudh1601 <ranirudhan01@gmail.com>
2024-06-10 17:19:03 +0530

    docker

--- hello/settings.py
+++ hello/settings.py
@@ -93,10 +93,10 @@ DB_IS_AVAIL = all([
     DB_HOST,
     DB_PORT
 ])
-POSTGRES_READY = str(os.environ.get("POSTGRES_READY")) == "1"
+# POSTGRES_READY = str(os.environ.get("POSTGRES_READY")) == "1"
 
 
-if DB_IS_AVAIL and POSTGRES_READY:
+if DB_IS_AVAIL :
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
@@ -108,7 +108,7 @@ if DB_IS_AVAIL and POSTGRES_READY:
         }
     }
 
-print(DATABASES)
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
--- migrate.sh
+++ migrate.sh
@@ -4,6 +4,6 @@ DJANGO_SUPERUSER_EMAIL = ${DJANGO_SUPERuSER_EMAIL:-"hello@hello.com"}
 cd /app/
 
 /opt/venv/bin/python manage.py migrate --noinput 
-
+/opt/venv/bin/python manage.py collectstatic --noinput
 /opt/venv/bin/python manage.py createsuperuser --email ${DJANGO_SUPERUSER_EMAIL} --noinput || true
 
